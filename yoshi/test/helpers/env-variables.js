d7754cb14d6ac2e66dc52a1db0ec9a8c7b6f3c7c
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-07-05 11:25:12 +0300

    Update env-variables.js

--- yoshi/test/helpers/env-variables.js
+++ yoshi/test/helpers/env-variables.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   outsideTeamCity: {TEAMCITY_VERSION: '', BUILD_NUMBER: ''},
-  insideTeamCity: {TEAMCITY_VERSION: 1},
+  insideTeamCity: {BUILD_NUMBER: 1},
   insideWatchMode: {WIX_NODE_BUILD_WATCH_MODE: true},
   teamCityArtifactVersion: {ARTIFACT_VERSION: '1.0.0'},
   noArtifactVersion: {ARTIFACT_VERSION: ''}
