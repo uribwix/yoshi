f0611fa175236f0eb00abd65af6a88ed933b4003
Shahar Talmi <shahart@wix.com>
2017-07-26 17:56:52 +0300

    adding back the hash prefix

--- yoshi-runtime/index.js
+++ yoshi-runtime/index.js
@@ -1,5 +1,6 @@
 const {isProduction, isCI} = require('yoshi-utils');
 const genericNames = require('generic-names');
+const path = require('path');
 
 const patterns = module.exports.patterns = {
   long: '[path][name]__[local]__[hash:base64:5]',
@@ -15,10 +16,12 @@ module.exports.wixCssModulesRequireHook = (rootDir = './dist/src') => {
   require('css-modules-require-hook')({
     rootDir,
     generateScopedName: (name, filepath) => {
-      let generate = genericNames(cssModulesPattren(), {context: rootDir});
+      let context = rootDir;
       if (filepath.indexOf('/node_modules/') > -1) {
-        generate = genericNames(cssModulesPattren(), {context: rootDir.replace('/src', '')});
+        context = context.replace('/src', '');
       }
+      const hashPrefix = require(path.resolve('package.json')).name;
+      const generate = genericNames(cssModulesPattren(), {context, hashPrefix});
       return generate(name, filepath);
     },
     extensions: ['.scss', '.css', '.less', '.sass'],
