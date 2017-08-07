589f62101684327eb022547c2cf807bac7056c4c
Yarden Sod-Moriah <yardnsm@gmail.com>
2017-08-07 18:22:14 +0300

    yoshi: run js files only in wallaby setup step (#237)

--- yoshi/config/test-setup.js
+++ yoshi/config/test-setup.js
@@ -3,13 +3,14 @@
 const path = require('path');
 const {tryRequire, isTypescriptProject} = require('../lib/utils');
 
-const ext = isTypescriptProject() ? 'ts' : 'js';
-const mochSetupPath = path.join(process.cwd(), 'test', `mocha-setup.${ext}`);
+const ext = isTypescriptProject() && !process.env.IN_WALLABY ? 'ts' : 'js';
+const mochaSetupPath = path.join(process.cwd(), 'test', `mocha-setup.${ext}`);
 const setupPath = path.join(process.cwd(), 'test', `setup.${ext}`);
 
 if (!process.env.IN_WALLABY) {
   require('../lib/require-hooks');
 }
+
 require('../lib/ignore-extensions');
-tryRequire(mochSetupPath);
+tryRequire(mochaSetupPath);
 tryRequire(setupPath);
