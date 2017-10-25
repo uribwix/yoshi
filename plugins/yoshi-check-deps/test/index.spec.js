89c3d1f7ba51abc765fc960dc1515ecf17297d59
Tomas Miliauskas <tomas@wix.com>
2017-10-25 10:26:37 +0300

    chore(temp-fix): remove

--- plugins/yoshi-check-deps/test/index.spec.js
+++ plugins/yoshi-check-deps/test/index.spec.js
@@ -77,15 +77,5 @@ describe('yoshi-deps', () => {
         return acc;
       }, {})
     });
-
-    npmServer.get('/yoshi').reply(200, {
-      _id: 'yoshi',
-      name: 'yoshi',
-      'dist-tags': {latest: versions.slice().pop()},
-      versions: versions.reduce((acc, ver) => {
-        acc[ver] = {};
-        return acc;
-      }, {})
-    });
   }
 });
